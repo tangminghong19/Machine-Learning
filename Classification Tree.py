029bf84c184d474a8f44be6ed682fe9c2daf52ef
Ming Hong <tangmh17@gmail.com>
2025-12-07 16:33:35 +0800

    update

--- Classification Tree.py
+++ Classification Tree.py
@@ -41,3 +41,85 @@ df.dtypes
 
 #print out unique values in 'ca' column
 df['ca'].unique()
+df['thal'].unique()
+
+# sklearn's classification trees do not support dataset with missing values
+# so, we can either delete those missing data from the training set, or impute values for the missing data
+
+# len() prints out the number of rows
+# loc[], short for "location", allows us to specify which rows we want
+# so we say we want any row with '?' in the 'ca' column
+# OR
+# any row with '?' in the 'thal' column
+len(df.loc[(df['ca'] == '?') | (df['thal'] == '?')])
+
+# print out the rows with missing data
+df.loc[(df['ca'] == '?') | (df['thal'] == '?')]
+
+len(df)
+
+# 6 of the 303 rows, or 2%, contain missing values.
+# Since 303 - 6 = 297 is still plenty of data to train a classification tree,
+# we will simply delete those rows with missing data,
+# rather than trying to impute values for the missing data.
+
+# select all rows that do not contain missing values
+# and save them in a new dataframe called "df_no_missing"
+
+df_no_missing = df.loc[(df['ca'] != '?') & (df['thal'] != '?')]
+
+len(df_no_missing)
+df_no_missing['ca'].unique()
+df_no_missing['thal'].unique()
+
+# ca and thal columns are still object datatypes
+
+### Split the data into dependent and independent variables
+# use X to represent the columns of data that we'll use to make classifications
+# use y to represent the column of data that we want to predict
+# deal with missing data before splitting into X and y to ensure that each row in X correctly corresponds 
+# with the appropriate value in y
+
+# by default, pandas uses copy by REFERENCE
+# using copy() to copy the data by VALUE
+# to ensure that the original data df_no_missing is not modified when we modify X and y
+
+X = df_no_missing.drop('hd', axis=1).copy() # alternatively: X = df_no_missing.iloc[:, :-1]
+X.head()
+
+y = df_no_missing['hd'].copy() # alternatively: y = df_no_missing.iloc[:, -1]
+
+# continue formatting X so that it's suitable for making a Decision Tree
+
+### One-Hot Encoding
+X.dtypes
+
+# scikit-learn decision trees natively support continuous data, but not categorical data (eg. cp)
+# thus we have to convert a column of categorical data into multiple columns of binary values.
+# One-Hot Encoding!
+
+# If we treat the four options for cp as continuous data,
+# we'd assume that 4 is more similar to 3 than it is to 1 or 2.
+# That menas the decision tree would be more likely to cluster the patients with 4s and 3s together
+# than the patients with 4s and 1s together.
+# If we treat the four options for cp as categorical data,
+# then we treat each one as a separate category that is no more or less similar to any of the other categories.
+# Thus, the likelihood of the decision tree clustering patients with 4s and 3s together
+# is the same as the likelihood of the decision tree clustering patients with 4s and 1s together.
+# This approach makes more sense for categorical data.
+
+X['cp'].unique()
+
+## Ways to do One-Hot Encoding in Python:
+## 1. scikit-learn.ColumnTransformer()
+# It creates a persistent function that can validate data that we get in the future.
+# Eg. If we build a decision tree using a categorical variable favourite colour that has red, blue, and green options,
+# then ColumnTransformer() can remember those options and later on when the decision tree is used in a production system,
+# if someone says their favourite colour is yellow,
+# ColumnTransformer() can throw an error or handle it in some other way.
+# However, it turns the data into an array and looses all of the column names,
+# making it harder to verify that the usage of ColumnTransformer() worked as we intended it to.
+## 2. pandas.get_dummies()
+# It leaves the data in a dataframe and retains the column names,
+# making it much easier to verify that it worked as intended.
+# However, it does not have the persistent behaviour of ColumnTransformer().
